dbac77c69a311456c6da4ef7ab975e30d5a84dc5
Captain <pdalavi55@gmail.com>
2020-05-27 11:50:12 +0530

    Initial Project Files

---
+++ src/main/java/com/testing_example/string_calculator/StringCalculator.java
@@ -0,0 +1,10 @@
+package com.testing_example.string_calculator;
+
+public class StringCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/testing_example/string_calculator/TDD_TestingStringCalculator.java
@@ -0,0 +1,14 @@
+package com.testing_example.string_calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TDD_TestingStringCalculator {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
